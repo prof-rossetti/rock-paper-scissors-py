908325511dd422e592cd2b2fc441d406954956b2
Mike Rossetti <s2t2@users.noreply.github.com>
2019-05-28 16:07:48 -0400

    Capture user selection from listbox

--- app/sg_game.py
+++ app/sg_game.py
@@ -1,33 +1,32 @@
-# adapted from notes: https://github.com/prof-rossetti/nyu-info-2335-201905/blob/master/notes/python/packages/pysimplegui.md
+# credits:
+#  + https://github.com/prof-rossetti/nyu-info-2335-201905/blob/master/notes/python/packages/pysimplegui.md
+#  + https://pysimplegui.readthedocs.io/en/latest/tutorial/
+#  + https://github.com/PySimpleGUI/PySimpleGUI/blob/master/PySimpleGUI.py#L775-L876
 
 import PySimpleGUI as sg
 
 #from game import random_choice, determine_winner, WIN_MESSAGE, LOSE_MESSAGE, TIE_MESSAGE
 
-
-
-
 #sg.Popup("Hello From PySimpleGUI!", "This is the shortest GUI program ever!")
 
-
 layout = [
-    [sg.Text("Enter your name"), sg.InputText()],
-    [sg.OK()]
+    [
+        sg.Text("Please choose an option from the dropdown:")
+    ],
+    [
+        #sg.Listbox(values=("rock", "paper", "scissors"), size=(30, 3))
+        sg.Listbox(values=("rock", "paper", "scissors"), size=(30, 3), select_mode="single")
+    ],
+    [
+        sg.Submit()
+    ]
 ]
 
 window = sg.Window("My first GUI").Layout(layout)
 
-# button, (name,) = window.Read()
-# print("NAME:", name) # name is zero
-
-#breakpoint()
-
-# type(window) #> <class 'PySimpleGUI.PySimpleGUI.Window'>
-
 button, values = window.Read()
 
-#print("BUTTON:", button) #> "OK"
-#print(type(values)) #> dict
-#print("VALUES:", values) #> VALUES: {0: 'Polly Professor'}
+#print(values) #> {0: ['scissors']}
+#print(values[0]) #> ['scissors']
 
-print("NAME:", values[0])
+print("USER SELECTION:", values[0][0])
