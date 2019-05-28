73ae753d849cb75a772e206d5946796b3133a76e
Mike Rossetti <s2t2@users.noreply.github.com>
2019-05-28 13:46:09 -0400

    Demonstrate ability to read textbox inputs

--- app/sg_game.py
+++ app/sg_game.py
@@ -4,4 +4,30 @@ import PySimpleGUI as sg
 
 #from game import random_choice, determine_winner, WIN_MESSAGE, LOSE_MESSAGE, TIE_MESSAGE
 
-sg.Popup("Hello From PySimpleGUI!", "This is the shortest GUI program ever!")
+
+
+
+#sg.Popup("Hello From PySimpleGUI!", "This is the shortest GUI program ever!")
+
+
+layout = [
+    [sg.Text("Enter your name"), sg.InputText()],
+    [sg.OK()]
+]
+
+window = sg.Window("My first GUI").Layout(layout)
+
+# button, (name,) = window.Read()
+# print("NAME:", name) # name is zero
+
+#breakpoint()
+
+# type(window) #> <class 'PySimpleGUI.PySimpleGUI.Window'>
+
+button, values = window.Read()
+
+#print("BUTTON:", button) #> "OK"
+#print(type(values)) #> dict
+#print("VALUES:", values) #> VALUES: {0: 'Polly Professor'}
+
+print("NAME:", values[0])
