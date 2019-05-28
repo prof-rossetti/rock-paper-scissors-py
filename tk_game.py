471db5965a75448e76d14cdd5ca04be41c12e140
Mike Rossetti <s2t2@users.noreply.github.com>
2019-05-28 12:25:11 -0400

    Display results in a message box

--- tk_game.py
+++ tk_game.py
@@ -1,6 +1,8 @@
 # adapted from notes: https://github.com/prof-rossetti/nyu-info-2335-201905/blob/master/notes/python/packages/tkinter.md
 
 import tkinter
+#import tkMessageBox #> ModuleNotFoundError: No module named 'tkMessageBox'
+import tkinter.messagebox # h/t: https://stackoverflow.com/a/38181986/670433
 
 window = tkinter.Tk()
 
@@ -15,6 +17,8 @@ my_select.insert(3, "scissors")
 def handle_button_click():
     print("------------------------------")
     print("You chose:", my_select.get(my_select.curselection()))
+    #tkMessageBox.showinfo("Congratulations", "You won!")
+    tkinter.messagebox.showinfo("Congratulations", "You won!")
 
 my_button = tkinter.Button(text="Click Me", command=handle_button_click)
 
